84f49d75eb757da29dee4737ed6733b018afea93
Kangnets <kangnets88@gmail.com>
2024-08-04 05:31:29 +0900

    fix : 갑자기 온보딩 흰화면

--- src/pages/onboarding/page.tsx
+++ src/pages/onboarding/page.tsx
@@ -63,7 +63,7 @@ export function Onboarding() {
   };
 
   return (
-    <div className="main flex flex-col min-h-screen overflow-hidden">
+    <div className="main flex flex-col h-screen overflow-hidden">
       <div className="main_content flex-1 flex flex-col">
         {renderComponent()}
         <div className="flex-grow"></div>{" "}
