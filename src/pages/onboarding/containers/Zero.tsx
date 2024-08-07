8b073fde47a02760fc9646576df5b9a04fbcc9a0
Kangnets <kangnets88@gmail.com>
2024-08-07 17:33:38 +0900

    카카오 로그인 fix 8

--- src/pages/onboarding/containers/Zero.tsx
+++ src/pages/onboarding/containers/Zero.tsx
@@ -18,19 +18,20 @@ const Zero: React.FC<ZeroProps> = () => {
   const Rest_api_key = import.meta.env.VITE_KAKAO_REST_API_KEY; // REST API KEY
   const redirect_uri = import.meta.env.VITE_KAKAO_REDIRECT_URI;
 
-  useEffect(() => {
-    if (!window.Kakao.isInitialized()) {
-      window.Kakao.init(Rest_api_key);
-    }
-  }, [Rest_api_key]);
-
   const handleLogin = () => {
+    const webLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${encodeURIComponent(
+      redirect_uri
+    )}&response_type=code`;
+
+    // Try to redirect using the intent URL
+    // Fallback to web login if the intent URL fails
+    window.location.href = webLoginUrl;
+
     window.Kakao.Auth.authorize({
       redirectUri: redirect_uri,
       throughTalk: true, // 간편 로그인을 위해 추가
     });
   };
-
   return (
     <div className="flex flex-col min-h-screen">
       <OnboardingTitle />
