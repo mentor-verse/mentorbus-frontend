b6d2b31a9577ec5255c2b7ed17c05fbe37878633
Kangnets <kangnets88@gmail.com>
2024-08-08 19:47:03 +0900

    fix : 화면 깨짐 수정 3

--- src/pages/mentorbus/page.tsx
+++ src/pages/mentorbus/page.tsx
@@ -174,7 +174,7 @@ export function MentorBusPage() {
             </div>
           </div>
 
-          <div ref={growDivRef}></div>
+          <div className="bg-white" ref={growDivRef}></div>
 
           <BottomNav />
         </div>
