66312149ef54daa241e350ca1be4c25040428ac7
Kangnets <kangnets88@gmail.com>
2024-08-11 13:26:20 +0900

    fix : UI 에러 수정 5

--- src/pages/findmentor/containers/NotYetPage.tsx
+++ src/pages/findmentor/containers/NotYetPage.tsx
@@ -1,21 +1,43 @@
+import React, { useEffect, useRef } from "react";
 import { cn } from "@/libs/utils.ts";
-import React from "react";
 import FindTitle from "../containers/FindTitle";
 import BottomNav from "@/containers/navbar";
 
 export interface NotYetPageProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const NotYetPage = React.forwardRef<HTMLDivElement, NotYetPageProps>(
-  ({ className }) => {
+  ({ className }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const updateHeight = () => {
+        if (containerRef.current) {
+          containerRef.current.style.height = `${window.innerHeight}px`;
+        }
+      };
+
+      // Set initial height
+      updateHeight();
+
+      // Update height on resize
+      window.addEventListener("resize", updateHeight);
+
+      // Clean up event listener on unmount
+      return () => window.removeEventListener("resize", updateHeight);
+    }, []);
+
     return (
-      <div className={cn("relative min-h-screen flex flex-col", className)}>
+      <div
+        className={cn("relative flex flex-col", className)}
+        ref={containerRef}
+      >
         <FindTitle
           title="멘토 찾기"
           Link={""}
           back_disable={"no"}
           back_work={"no"}
         />
-        <div className="flex-grow flex justify-center items-center bg-white">
+        <main className="flex flex-1 justify-center items-center bg-white">
           <div className="text-center text-[#333333]">
             <div className="text-[79px] mb-4">🚧</div>
             <div className="text-[19px] font-bold mb-2">업데이트 예정</div>
@@ -23,7 +45,7 @@ const NotYetPage = React.forwardRef<HTMLDivElement, NotYetPageProps>(
               앞으로의 멘토버스를 기대해주세요!
             </div>
           </div>
-        </div>
+        </main>
         <BottomNav />
       </div>
     );
