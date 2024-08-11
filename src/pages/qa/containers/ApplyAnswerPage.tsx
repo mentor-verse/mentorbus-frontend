876bd768fc43f93c32f13fd84622fbc8651694b7
Kangnets <kangnets88@gmail.com>
2024-08-11 10:59:26 +0900

    fix : UI 에러 수정

--- src/pages/qa/containers/ApplyAnswerPage.tsx
+++ src/pages/qa/containers/ApplyAnswerPage.tsx
@@ -109,7 +109,7 @@ const ApplyAnswerPage = React.forwardRef<HTMLDivElement, ApplyAnswerPageProps>(
               </div>
               <textarea
                 className={cn(
-                  "text-[13px] text-[#383838] font-normal mt-[90px] flex justify-start ml-[33px] w-[80%] border-none focus:outline-none h-[50vh]",
+                  "text-[13px] text-[#383838] font-normal mt-[90px] flex justify-start ml-[33px] w-[80%] border-none focus:outline-none resize-none min-h-[50px] max-h-[500px] overflow-y-auto",
                   className
                 )}
                 placeholder="답변을 등록하세요"
