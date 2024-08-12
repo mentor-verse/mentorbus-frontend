ed03fb3d5dfefdc68e28eb50ebf2d19810ac6827
Kangnets <kangnets88@gmail.com>
2024-08-13 08:49:56 +0900

    fix : 온보딩 분기 5

--- src/pages/findmentor/containers/ApplyFinished.tsx
+++ src/pages/findmentor/containers/ApplyFinished.tsx
@@ -10,8 +10,8 @@ export function ApplyFinished() {
         <div className="grid place-items-center">
           <Rocket />
         </div>
-        <div className=" text-lg not-italic font-medium text-[16px] mt-[20px]">
-          <p className="flex justify-center w-[330px] tracking-[-0.1em]">
+        <div className=" text-lg not-italic font-medium text-[14px] mt-[10px]">
+          <p className="flex justify-center w-[330px] text-[14px] tracking-[-0.1em]">
             멘토링 신청 정보는 &nbsp;
             <p className="font-bold text-[#1FA7F2]">멘토버스</p>
             에서 확인 가능합니다
