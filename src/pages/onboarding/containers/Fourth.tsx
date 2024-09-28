6a8af3b5003569e71f834c7c4df868bad5b9921b
Kangnets <kangnets88@gmail.com>
2024-09-29 02:00:43 +0900

    메인 정보 없으면 온보딩으로 보내기 5

--- src/pages/onboarding/containers/Fourth.tsx
+++ src/pages/onboarding/containers/Fourth.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "@/components/Icons/Logo";
 import { OnboardingButton } from "@/components/ui/onboardingbutton";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface FourthProps {
@@ -10,14 +11,61 @@ interface FourthProps {
 
 export function Fourth({ count, setCount, sentence }: FourthProps) {
   const navigate = useNavigate();
+  const [kakaoId, setKakaoId] = useState<string | null>(null); // kakaoId 상태값으로 설정
 
-  const handleNext = (major: string) => {
+  // URL에서 kakaoId를 가져오는 함수
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const urlKakaoId = searchParams.get("userId"); // URL에서 userId 파라미터로 kakaoId 추출
+    if (urlKakaoId) {
+      setKakaoId(urlKakaoId); // 상태 업데이트
+      localStorage.setItem("kakao_id", urlKakaoId); // localStorage에 저장
+    } else {
+      const storedKakaoId = localStorage.getItem("kakao_id");
+      if (storedKakaoId) {
+        setKakaoId(storedKakaoId);
+      }
+    }
+  }, [location.search]);
+
+  const handleNext = () => {
     localStorage.setItem("major", major); // Store the major in localStorage
     const position = localStorage.getItem("position");
+    const userName = localStorage.getItem("userName");
+    console.log("userName:", userName);
+    const userBelong = localStorage.getItem("userBelong");
+    const major = localStorage.getItem("major");
 
     if (position === "멘토") {
-      const userName = localStorage.getItem("userName");
-      navigate(`/mentorbus-frontend/main?userName=${userName}`);
+      // Mentor data를 POST
+      const mentorData = {
+        nickname: userName,
+        position: position,
+        job: userBelong,
+        major: major,
+        kakao_id: kakaoId,
+      };
+
+      console.log("mentorData:", mentorData);
+
+      fetch(
+        "https://port-0-mentorbus-backend-m0zjsul0a4243974.sel4.cloudtype.app/onboarding/mentor",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(mentorData),
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("User data saved:", data);
+          navigate(`/mentorbus-frontend/main?userId=${kakaoId}`);
+        })
+        .catch((error) => {
+          console.error("Error saving mentor data:", error);
+        });
     }
   };
 
