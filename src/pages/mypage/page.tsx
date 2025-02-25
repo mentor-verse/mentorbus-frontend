99a8dabef93d313609e56f52bbf47fedeaeb4b0d
Kangnets <kangnets88@gmail.com>
2025-02-25 19:54:42 +0900

    [refactor] api 코드 리펙토링

--- src/pages/mypage/page.tsx
+++ src/pages/mypage/page.tsx
@@ -13,88 +13,54 @@ import { useEffect, useState } from "react";
 import { Cloud } from "@/components/Icons/Cloud";
 import { Cloud2 } from "@/components/Icons/Cloud2";
 import BottomNav from "@/containers/navbar";
-import { useGetProfile } from "@/hooks/useGetProfile";
-import { useQueryClient } from "@tanstack/react-query";
-import { getProfileRes } from "@/types/get";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { UserData } from "@/types/login/UserType";
 
 export function MyPage() {
-  const queryClient = useQueryClient();
-  const [level] = useState("3"); // 기본값을 설정하거나 필요에 따라 변경 가능
-  const [userData, setUserData] = useState<getProfileRes | null>(null);
-
-  const userId = 3645188105;
-  const { data: resp, isLoading, isError, refetch } = useGetProfile({ userId });
+  const user = useSelector((state: RootState) => state.user);
+  const [userData, setUserData] = useState<UserData | null>(user || null);
 
   useEffect(() => {
-    if (!queryClient.getQueryData(["get-profile"])) {
-      refetch();
+    if (Array.isArray(user) && user.length > 0) {
+      setUserData(user[0]);
+      console.log("얍");
+    } else {
+      setUserData(null);
     }
-  }, [queryClient, refetch]);
+  }, [user]);
 
-  useEffect(() => {
-    if (resp) {
-      setUserData(resp);
-    }
-  }, [resp]);
+  console.log("userData", userData);
 
   const renderLevelIcon = () => {
     if (userData?.isMentor === true) {
-      switch (level) {
-        case "1":
+      switch (userData?.level) {
+        case 1:
           return <LevelFirstMentor />;
-        case "2":
+        case 2:
           return <LevelSecondMentor />;
-        case "3":
+        case 3:
           return <LevelThirdMentor />;
-        case "4":
+        case 4:
           return <LevelFourthMentor />;
         default:
           return null;
       }
     } else {
-      switch (level) {
-        case "1":
+      switch (userData?.level) {
+        case 1:
           return <LevelFirst />;
-        case "2":
+        case 2:
           return <LevelSecond />;
-        case "3":
+        case 3:
           return <LevelThird />;
-        case "4":
+        case 4:
           return <LevelFourth />;
         default:
           return null;
       }
     }
   };
-
-  if (isLoading) {
-    return (
-      <div
-        style={{
-          color: "#888888",
-          fontSize: "25px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center", // 수평 중앙 정렬
-          justifyContent: "center", // 수직 중앙 정렬
-          height: "50vh", // 화면 전체 높이 설정
-        }}
-      >
-        <div
-          style={{
-            width: "800px",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          ⏰
-        </div>
-        로딩 중입니다<br></br>잠시 기다려주세요
-      </div>
-    );
-  }
-
-  if (isError) return <p>에러발생 삐용삐용</p>;
 
   return (
     <>
@@ -115,7 +81,7 @@ export function MyPage() {
               <Profile
                 name={userData?.userName || "Unknown"}
                 school={userData?.job || "Unknown School"}
-                level={"3"}
+                level={userData?.level}
                 gen="man"
               />
             </div>
